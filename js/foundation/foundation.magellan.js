e5ada3482ce4d84a49bfe8c7a1e76fe423dca139
bioavatar <outlook.com>
2014-07-22 02:49:42 -0700

    clean

--- js/foundation/foundation.magellan.js
+++ js/foundation/foundation.magellan.js
@@ -10,7 +10,7 @@
       active_class: 'active',
       threshold: 0, // pixels from the top of the expedition for it to become fixes
       destination_threshold: 20, // pixels from the top of destination for it to be considered active
-      throttle_delay: 30, // calculation throttling to increase framerate
+      throttle_delay: 40, // calculation throttling to increase framerate
       fixed_top: 0 // top distance in pixels assigend to the fixed element on scroll
     }, 
 
